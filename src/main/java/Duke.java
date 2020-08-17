abedca29ae884ef6b884c35dd0fb94d1b9b11108
amalinasani <nuramalina.mdsani@gmail.com>
2020-08-17 21:17:02 +0800

    Add Level-0

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,6 @@
 public class Duke {
+    public static final String LINE_HEADER = "---------------------------------";
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -6,5 +8,17 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        greet();
+        goodbye();
+    }
+
+    static void greet(){
+        System.out.println(LINE_HEADER);
+        System.out.println("Hello! I'm Duke\nWhat can I do for you?\n");
+    }
+
+    static void goodbye(){
+        System.out.println(LINE_HEADER);
+        System.out.println("Bye. Hope to see you again soon!\n");
     }
 }
