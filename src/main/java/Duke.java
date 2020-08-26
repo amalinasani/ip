1d4a4fc45173febc9beb9d7bbd2cb7d0f3cfc1cb
amalinasani <nuramalina.mdsani@gmail.com>
2020-08-26 20:45:46 +0800

    Add Level-2
    Implement addToList method and updated listAllTasks method to display all added tasks.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,6 +1,11 @@
 import java.util.Scanner;
+import java.util.Arrays;
+import java.lang.String;
 
 public class Duke {
+    public static String[] toDoList = new String[100];
+    public static int taskCount = 0;
+
     public static final String LINE_HEADER = "\t---------------------------------------";
 
     public static void main(String[] args) {
@@ -14,11 +19,14 @@ public class Duke {
         greet();
 
         inputCommand = getCommand();
+
         do {
             if (inputCommand.equals("list")) {
                 listAllTasks();
             } else if (inputCommand.equals("blah")) {
                 blah();
+            } else if (inputCommand.equals("add")) {
+                addToList();
             }
             inputCommand = getCommand();
         } while(!inputCommand.equals("bye"));
@@ -42,7 +50,13 @@ public class Duke {
     static void listAllTasks(){
         System.out.println("list");
         System.out.println(LINE_HEADER);
-        System.out.println("\tlist");
+        int taskNumber = 1;
+
+        for (String task: Arrays.copyOf(toDoList, taskCount)){
+            System.out.println(taskNumber + ". " + task);
+            taskNumber++;
+        }
+
         System.out.println(LINE_HEADER);
     }
 
@@ -57,6 +71,21 @@ public class Duke {
         System.out.println("bye");
         System.out.println(LINE_HEADER);
         System.out.println("\tBye. Hope to see you again soon!\n");
+        System.out.println(LINE_HEADER);
+    }
+
+    static void addToList(){
+        String task;
+
+        Scanner in = new Scanner(System.in);
+        task = in.nextLine();
+
+        System.out.println(LINE_HEADER);
+        System.out.println("added: " + task);
+
+        toDoList[taskCount] = task;
+        taskCount++;
+
         System.out.println(LINE_HEADER);
     }
 }
