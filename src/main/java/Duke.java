2d91935778f3be4196848e6f7695e2613a70c0b9
amalinasani <nuramalina.mdsani@gmail.com>
2020-08-26 19:51:19 +0800

    Add Level-1
    Implement skeletal version of Duke that greats user, echoes commands entered by user, and exits when user types bye.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,7 +1,10 @@
+import java.util.Scanner;
+
 public class Duke {
-    public static final String LINE_HEADER = "---------------------------------";
+    public static final String LINE_HEADER = "\t---------------------------------------";
 
     public static void main(String[] args) {
+        String inputCommand;
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
@@ -9,16 +12,51 @@ public class Duke {
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
         greet();
+
+        inputCommand = getCommand();
+        do {
+            if (inputCommand.equals("list")) {
+                listAllTasks();
+            } else if (inputCommand.equals("blah")) {
+                blah();
+            }
+            inputCommand = getCommand();
+        } while(!inputCommand.equals("bye"));
+
         goodbye();
+    }
+
+    static String getCommand(){
+        String inputCommand;
+        Scanner in = new Scanner(System.in);
+        inputCommand = in.nextLine();
+        return inputCommand;
     }
 
     static void greet(){
         System.out.println(LINE_HEADER);
-        System.out.println("Hello! I'm Duke\nWhat can I do for you?\n");
+        System.out.println("\tHello! I'm Duke\n\tWhat can I do for you?\n");
+        System.out.println(LINE_HEADER);
+    }
+
+    static void listAllTasks(){
+        System.out.println("list");
+        System.out.println(LINE_HEADER);
+        System.out.println("\tlist");
+        System.out.println(LINE_HEADER);
+    }
+
+    static void blah(){
+        System.out.println("blah");
+        System.out.println(LINE_HEADER);
+        System.out.println("\tblah");
+        System.out.println(LINE_HEADER);
     }
 
     static void goodbye(){
+        System.out.println("bye");
         System.out.println(LINE_HEADER);
-        System.out.println("Bye. Hope to see you again soon!\n");
+        System.out.println("\tBye. Hope to see you again soon!\n");
+        System.out.println(LINE_HEADER);
     }
 }
