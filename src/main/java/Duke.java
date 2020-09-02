9de917d46cd6db3eb675fea1cd966695fa84e59d
amalinasani <nuramalina.mdsani@gmail.com>
2020-09-02 20:27:05 +0800

    Improve code naming

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,27 +3,22 @@ import java.util.Arrays;
 import java.lang.String;
 
 public class Duke {
-    public static Task[] toDoList = new Task[100];
+    public static Task[] taskList = new Task[100];
     public static int taskCount = 0;
-    public static final String LINE_HEADER = "\n▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄\n";
+    public static final String LINE_HEADER = "\n=====================================================\n";
 
     public static void main(String[] args) {
         String inputCommand;
-        String logo = " _      _       ____  ___ ___   ____ \n"
-                + "| |    | |     /    ||   |   | /    |\n"
-                + "| |    | |    |  o  || _   _ ||  o  |\n"
-                + "| |___ | |___ |     ||  \\_/  ||     |\n"
-                + "|     ||     ||  _  ||   |   ||  _  |\n"
-                + "|     ||     ||  |  ||   |   ||  |  |\n"
-                + "|_____||_____||__|__||___|___||__|__|";
-        System.out.println("Hello from\n" + logo);
         printGreeting();
 
+        // Command handler
         do {
-            inputCommand = getCommand();
-            Scanner taskObj = new Scanner(inputCommand);
+            Scanner in = new Scanner(System.in);
+            inputCommand = in.nextLine();
 
-            switch (taskObj.next()){
+            String taskObj = new Scanner(inputCommand).next();
+
+            switch (taskObj){
                 case "list":
                     listAllTasks();
                     break;
@@ -58,20 +53,44 @@ public class Duke {
         } while (!inputCommand.equals("bye"));
     }
 
-    static String getCommand(){
-        Scanner in = new Scanner(System.in);
-        return in.nextLine();
-    }
 
+    // Prints greeting and logo
     static void printGreeting(){
+        String logo =     "  ____  ____   _      _       ____  ___ ___   ____ \n"
+                        + " /    ||    \\ | |    | |     /    ||   |   | /    |\n"
+                        + "|  o  ||  _  || |    | |    |  o  || _   _ ||  o  |\n"
+                        + "|     ||  |  || |___ | |___ |     ||  \\_/  ||     |\n"
+                        + "|  _  ||  |  ||     ||     ||  _  ||   |   ||  _  |\n"
+                        + "|  |  ||  |  ||     ||     ||  |  ||   |   ||  |  |\n"
+                        + "|__|__||__|__||_____||_____||__|__||___|___||__|__|";
+        System.out.println(LINE_HEADER + "\t\t\t\t\tHello from\n" + logo);
         System.out.println(LINE_HEADER+ "\tHello! I'm your friendly neighbourhood Llama.\n\tWhat can I do for you?" + LINE_HEADER);
     }
 
+    // Add task to taskList
+    static void addTask(Task task){
+        taskList[taskCount] = task;
+        taskCount++;
+        System.out.println(LINE_HEADER + "\tAdded: " + task + "\nNow you have " + taskCount + " task(s) in your list!" + LINE_HEADER);
+    }
+
+    // Marks task in taskList as done
+    static void markTaskAsDone(int taskNumber){
+        Task task = taskList[taskNumber];
+        if (task.isDone){
+            System.out.println(LINE_HEADER + "\tThis task has already been marked as done.");
+        } else {
+            task.markAsDone();
+            System.out.println(LINE_HEADER + "\tNice! I've marked this task as done:");
+        }
+        System.out.println("\t\t" + task  + LINE_HEADER);
+    }
+
+    // List all tasks in taskList
     static void listAllTasks(){
         int taskNumber = 1;
-
         System.out.println("list" + LINE_HEADER);
-        for (Task task: Arrays.copyOf(toDoList, taskCount)){
+        for (Task task: Arrays.copyOf(taskList, taskCount)){
             System.out.print(taskNumber + ". ");
             System.out.println(task);
             taskNumber++;
@@ -80,51 +99,31 @@ public class Duke {
     }
 
     static void printGoodbye(){
-        System.out.println("bye");
-        String logo = "                          ▓▓  ▓▓                                        \n" +
-                "                        ▓▓░░▓▓░░▓▓                                      \n" +
-                "                      ▓▓▓▓░░░░░░▓▓                                      \n" +
-                "                    ▓▓░░░░░░██░░▓▓                                      \n" +
-                "                    ▓▓░░░░░░░░░░▓▓                                      \n" +
-                "                      ▓▓▓▓░░░░░░▓▓                                      \n" +
-                "                          ▓▓░░░░▓▓                                      \n" +
-                "                          ▓▓░░░░▓▓                ▓▓                    \n" +
-                "                          ▓▓░░░░▓▓              ▓▓░░▓▓                  \n" +
-                "                          ▓▓░░░░▓▓              ▓▓░░▓▓                  \n" +
-                "                          ▓▓░░░░░░▓▓▓▓▓▓▓▓▓▓▓▓▓▓░░░░▓▓                  \n" +
-                "                          ▓▓░░░░░░░░░░░░░░░░░░░░░░▓▓                    \n" +
-                "                          ▓▓░░░░░░░░░░░░░░░░░░░░░░▓▓                    \n" +
-                "                          ▓▓░░░░░░░░░░░░░░░░░░░░░░▓▓                    \n" +
-                "                          ▓▓░░░░░░░░░░░░░░░░░░░░░░▓▓                    \n" +
-                "                          ▒▒░░▒▒░░▒▒▒▒░░░░░░░░░░▒▒▓▓                    \n" +
-                "                            ▓▓░░░░░░░░░░░░░░░░░░▓▓                      \n" +
-                "                            ▓▓▓▓▓▓░░▓▓▓▓▓▓▓▓▓▓░░▓▓                      \n" +
-                "                            ▓▓░░▓▓░░▓▓  ▓▓░░▓▓░░▓▓                      \n" +
-                "                            ▓▓░░▓▓░░▓▓  ▓▓░░▓▓░░▓▓                      \n" +
-                "                            ▓▓░░▓▓░░▓▓  ▓▓░░▓▓░░▓▓                      \n" +
-                "                            ▓▓░░▓▓░░▓▓  ▓▓░░▓▓░░▓▓                      \n" +
-                "                            ▓▓░░▓▓░░▓▓  ▓▓░░▓▓░░▓▓                      \n" +
-                "                              ▒▒  ▓▓      ▓▓  ▓▓  ";
+        String logo =
+                "\n ____                ▓▓  ▓▓                                \n" +
+                "|    \\            ▓▓░░▓▓░░▓▓                              \n" +
+                "|  o  )          ▓▓▓▓░░░░░░▓▓                              \n" +
+                "|     |        ▓▓░░░░░░██░░▓▓                              \n" +
+                "|  O  |        ▓▓░░░░░░░░░░▓▓                              \n" +
+                "|     |          ▓▓▓▓░░░░░░▓▓                              \n" +
+                "|_____|              ▓▓░░░░▓▓                              \n" +
+                " __ __               ▓▓░░░░▓▓                ▓▓            \n" +
+                "|  |  |              ▓▓░░░░▓▓              ▓▓░░▓▓          \n" +
+                "|  |  |              ▓▓░░░░▓▓              ▓▓░░▓▓          \n" +
+                "|  ~  |              ▓▓░░░░░░▓▓▓▓▓▓▓▓▓▓▓▓▓▓░░░░▓▓          \n" +
+                "|___, |              ▓▓░░░░░░░░░░░░░░░░░░░░░░▓▓            \n" +
+                "|     |              ▓▓░░░░░░░░░░░░░░░░░░░░░░▓▓            \n" +
+                "|____/               ▓▓░░░░░░░░░░░░░░░░░░░░░░▓▓            \n" +
+                "   ___               ▓▓░░░░░░░░░░░░░░░░░░░░░░▓▓            \n" +
+                "  /  _]              ▒▒░░▒▒░░▒▒▒▒░░░░░░░░░░▒▒▓▓            \n" +
+                " /  [_                 ▓▓░░░░░░░░░░░░░░░░░░▓▓              \n" +
+                "|    _]                ▓▓▓▓▓▓░░▓▓▓▓▓▓▓▓▓▓░░▓▓              \n" +
+                "|   [_                 ▓▓░░▓▓░░▓▓  ▓▓░░▓▓░░▓▓              \n" +
+                "|     |                ▓▓░░▓▓░░▓▓  ▓▓░░▓▓░░▓▓              \n" +
+                "|_____|                ▓▓░░▓▓░░▓▓  ▓▓░░▓▓░░▓▓              \n" +
+                "                         ▒▒  ▓▓      ▓▓  ▓▓  ";
 
-        System.out.println(LINE_HEADER + "\tGoodbye!\n" + logo + LINE_HEADER);
-    }
-
-    static void addTask(Task task){
-        toDoList[taskCount] = task;
-        taskCount++;
-        System.out.println(LINE_HEADER + "added: " + task + LINE_HEADER);
-    }
-
-    static void markTaskAsDone(int taskNumber){
-        Task task = toDoList[taskNumber];
-        if (task.isDone){
-            System.out.println(LINE_HEADER + "\tThis task has already been marked as done.");
-            System.out.println("\t\t[" + task.getStatusIcon() + "] " + task.description + LINE_HEADER);
-        } else {
-            task.markAsDone();
-            System.out.println(LINE_HEADER + "\tNice! I've marked this task as done:");
-            System.out.println("\t\t" + task  + LINE_HEADER);
-        }
+        System.out.println(LINE_HEADER + logo + LINE_HEADER);
     }
 }
 
